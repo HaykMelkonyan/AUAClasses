a46752c9a58cb16a857980cc2527740e510471f0
hmelkonyan <hayk.melkonyan1@tumo.org>
2023-10-08 16:50:32 +0400

    added coil dependency and Internet permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
     implementation ("androidx.compose.runtime:runtime-livedata:1.5.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
 
 
 
